112294af8e0ed340cb8adae4b0147dafff052b42
Klaus Kazlauskas <klausmachado@gmail.com>
2016-05-15 12:26:14 -0300

    RouteService created and implemented

--- app/diablo/services/diablo-api.service.ts
+++ app/diablo/services/diablo-api.service.ts
@@ -5,18 +5,21 @@ import { config } from './../config/diablo-api.config';
 import { ApiService } from './api.service';
 import { battleTagValidator } from './../validators/battle-tag.validator';
 import { LocaleService } from './locale.service';
+import { RouterService } from './router.service';
 
 @Injectable()
 export class DiabloApiService extends ApiService {
   private baseUrl : string;
   private apikey : string;
   private localeService : LocaleService;
+  private router : RouterService;
   
   constructor(protected http: Http, protected jsonp: Jsonp) {
     super(http, jsonp);
     this.localeService = new LocaleService();
     this.baseUrl = config.baseUrl;
     this.apikey = config.apikey;
+    this.router = new RouterService();
   }
   
   /**
@@ -46,7 +49,7 @@ export class DiabloApiService extends ApiService {
     };
     
     battleTagValidator.validate(battleTag);
-    let url = `profile/${battleTag}/`;
+    let url = this.router.get('CareerProfile', [{battleTag: battleTag}]);
     
     if (locale) {
       this.localeService.setLocale(locale);
---
+++ app/diablo/services/router.service.ts
@@ -0,0 +1,40 @@
+interface RouteInterface {
+  name: string,
+  route: string
+}
+
+interface RouteParamInterface {
+  name: string,
+  value: any
+}
+
+var routes : Array<RouteInterface> = [
+    {
+      name: 'CareerProfile',
+      route: '/profile/:battleTag/'
+    }
+];
+
+export class RouterService {
+  public get(routeName: string, params : Array<Object>) : string {
+    let tempRoute: string;
+    
+    routes.forEach((route) => {
+      if (route.name == routeName) {
+        tempRoute = route.route;
+        
+        params.forEach((param) => {
+          let paramName : string;
+          
+          for (paramName in param) {
+            
+            if (param.hasOwnProperty(paramName))
+              tempRoute = tempRoute.replace(':' + paramName, param[paramName]);
+          }
+        })
+      }
+    });
+    
+    return tempRoute;
+  }
+}
